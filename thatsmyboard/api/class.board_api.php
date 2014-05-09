6c879ffa23b753de1a87b5ced7f3e4b859eb08a1
Mark Steve Samson <hello@marksteve.com>
2014-05-10 02:32:01 +0800

    API

--- thatsmyboard/api/class.board_api.php
+++ thatsmyboard/api/class.board_api.php
@@ -3,6 +3,39 @@
  *  API class for Moom Manes' That's My Board
  *  Contains dummy stuff only
  */
+
+
+function generate_data($seed) {
+  srand($seed);
+  $rainfall = array();
+  $slope = array();
+  $rainfall_threshold = rand() * 0.00000001;
+  $slope_threshold = rand() * 0.00000001;
+  $base = time() - 30 * 24 * 60 * 60;
+  for ($i = 0; $i < 20; $i++) {
+    if (rand() * 0.0000000005 < 0.2) {
+      $c = rand() * 0.00000001;
+    } else {
+      $c = 10;
+    }
+    $rainfall[] = array(
+      'threshold' => $rainfall_threshold,
+      'x' => $base - ($i * 129600),
+      'y' => $c + rand() * 0.000000001,
+    );
+    $slope[] = array(
+      'threshold' => $slope_threshold,
+      'x' => $base - ($i * 129600),
+      'y' => $c + rand() * 0.000000001,
+    );
+  }
+  return array(
+    'rainfall' => $rainfall,
+    'slope' => $slope
+  );
+}
+
+
 class BoardApi {
   public static function version() {
     return '1.0';
@@ -10,63 +43,31 @@ class BoardApi {
 
   public function locations() {
     return array (
-      array(
-        'name' => 'Tublay',
-        'x' => '16.522999',
-        'y' => '120.61747'
+      array_merge(
+        generate_data(9087312),
+        array(
+          'name' => 'Tublay',
+          'x' => '16.522999',
+          'y' => '120.61747',
+        )
       ),
-      array(
-        'name' => 'Rizal',
-        'x' => '14.112104',
-        'y' => '121.394779'
+      array_merge(
+        generate_data(90281372),
+        array(
+          'name' => 'Rizal',
+          'x' => '14.112104',
+          'y' => '121.394779',
+        )
       ),
-      array(
-        'name' => 'Amulung',
-        'x' => '17.846754',
-        'y' => '121.729782'
+      array_merge(
+        generate_data(32091831),
+        array(
+          'name' => 'Amulung',
+          'x' => '17.846754',
+          'y' => '121.729782',
+        )
       )
     );
   }
 
-  public function slope($location) {
-    $seed = intval($location);
-    srand($seed);
-    $data = array();
-    $threshold = rand() * 0.00000001;
-    $base = time() - 30 * 24 * 60 * 60;
-    for ($i = 0; $i < 20; $i++) {
-      if (rand() * 0.0000000005 < 0.2) {
-        $c = rand() * 0.00000001;
-      } else {
-        $c = 10;
-      }
-      $data[] = array(
-        'threshold' => $threshold,
-        'x' => $base - ($i * 129600),
-        'y' => $c + rand() * 0.000000001,
-      );
-    }
-    return $data;
-  }
-
-  public function rainfall($location) {
-    $seed = intval($location);
-    srand($seed);
-    $data = array();
-    $threshold = rand() * 0.00000001;
-    $base = time() - 30 * 24 * 60 * 60;
-    for ($i = 0; $i < 20; $i++) {
-      if (rand() * 0.0000000005 < 0.2) {
-        $c = rand() * 0.00000001;
-      } else {
-        $c = 10;
-      }
-      $data[] = array(
-        'threshold' => $threshold,
-        'x' => $base - ($i * 129600),
-        'y' => $c + rand() * 0.000000001,
-      );
-    }
-    return $data;
-  }
 }
--- thatsmyboard/api/index.php
+++ thatsmyboard/api/index.php
@@ -8,8 +8,6 @@ include_once 'class.board_api.php';
 
 getApi()->get('/version.json', array('BoardApi', 'version'), EpiApi::external);
 getApi()->get('/locations.json', array('BoardApi', 'locations'), EpiApi::external);
-getApi()->get('/([^/]+)/slope.json', array('BoardApi', 'slope'), EpiApi::external);
-getApi()->get('/([^/]+)/rainfall.json', array('BoardApi', 'rainfall'), EpiApi::external);
 
 getRoute()->run();
 
